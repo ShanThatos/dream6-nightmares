8f6a6e1025212e0ef5bcd02f27124dcc45fbb38d
Neepsy <eric55664@gmail.com>
2022-09-12 20:36:55 -0500

    Cleaning up animation code and sorting assets

--- Ratpuncher/Assets/Scripts/PlayerAttackManager.cs
+++ Ratpuncher/Assets/Scripts/PlayerAttackManager.cs
@@ -7,20 +7,8 @@ public class PlayerAttackManager : MonoBehaviour
     [Tooltip("Animation for each attack in combo sequence")]
     public GameObject[] attacks;
 
-    [Tooltip("Animation lock time for each attack")]
-    public float[] animLockTimes;
-
-    [Tooltip("Time between attacks")]
-    public float attackDelay = .2f;
-
-    [Tooltip("Time before combo resets")]
-    public float comboTime = 0.65f;
-
     [Tooltip("Where the attack is spawned")]
     public Transform attackPoint;
-
-    [Tooltip("Allow attack queueing (button mashing vs rhythm")]
-    public bool queueAttacks;
 
     [Tooltip("Player sprite (used to figure out direction)")]
     public Transform sprite;
@@ -28,9 +16,6 @@ public class PlayerAttackManager : MonoBehaviour
     // Index of next attack in the array to use
     int comboStage;
 
-    float currComboTime;
-    float currAnimLock;
-    float currAttackDelay;
     bool attackQueued = false;
     bool isAnimLocked;
     PlayerMovement playerMovement;
@@ -39,47 +24,13 @@ public class PlayerAttackManager : MonoBehaviour
     void Start()
     {
         comboStage = 0;
-        currComboTime = 0;
-        currAnimLock = 0;
         playerMovement = gameObject.GetComponent<PlayerMovement>(); 
-
-        if(attacks.Length != animLockTimes.Length)
-        {
-            Debug.LogError("Ensure that each attack has an animation lock time!");
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        /* if(currComboTime >= 0)
-        {
-            currComboTime -= Time.deltaTime;
 
-            if(currComboTime < 0)
-            {
-                comboStage = 0;
-                playerMovement.setAttackState(0);
-            }
-        }
-
-        if(currAnimLock >= 0)
-        {
-            currAnimLock -= Time.deltaTime;
-            if(currAnimLock < 0)
-            {
-                
-            }
-        }
-
-        if(currAttackDelay >= 0)
-        {
-            currAttackDelay -= Time.deltaTime;
-            if (attackQueued && currAttackDelay < 0)
-            {
-                executeAttack();
-            }
-        } */
     }
 
     public bool executeAttack()
