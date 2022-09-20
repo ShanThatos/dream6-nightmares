1b35e973dac9d478ba2b00bbdd3f495a51ab9df9
Shanth Koka <shanthkoka@gmail.com>
2022-09-20 02:22:45 -0500

    updates

--- Ratpuncher/Assets/Levels/LevelOneBossFightCameraTrigger.cs
+++ Ratpuncher/Assets/Levels/LevelOneBossFightCameraTrigger.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class LevelOneBossFightCameraTrigger : MonoBehaviour
 {
 
-    public Vector2 playerCameraOffset = new Vector2(0, 2);
+    public Vector2 cameraOffset = new Vector2(0, 2);
     bool triggered = false;
     Vector2 oldCameraOffset;
 
     void OnTriggerEnter2D(Collider2D other) {
         if (LayerMask.LayerToName(other.gameObject.layer) == "Player" && !triggered) {
             triggered = true;
-            oldCameraOffset = MainCameraScript.instance.playerCameraOffset;
-            MainCameraScript.instance.playerCameraOffset = playerCameraOffset;
+            oldCameraOffset = MainCameraScript.instance.cameraOffset;
+            MainCameraScript.instance.cameraOffset = cameraOffset;
         }
     }
 
     void OnTriggerExit2D(Collider2D other) {
         if (LayerMask.LayerToName(other.gameObject.layer) == "Player" && triggered) {
             triggered = false;
-            MainCameraScript.instance.playerCameraOffset = oldCameraOffset;
+            MainCameraScript.instance.cameraOffset = oldCameraOffset;
         }
     }
 }
