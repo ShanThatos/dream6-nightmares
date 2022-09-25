5913ef5b886e22e3ccfa9c32fa520be7191088a3
Jiaxinhuang1 <60531514+Jiaxinhuang1@users.noreply.github.com>
2022-09-25 15:55:41 -0500

    somewhat transition?

--- Ratpuncher/Assets/Scripts/ScenesTransition.cs
+++ Ratpuncher/Assets/Scripts/ScenesTransition.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using UnityEngine.SceneManagement;
 
 public class ScenesTransition : MonoBehaviour
 {
+    private Animator transitionAnim;
+    public bool HasChangedScenes { get; private set; } // true if a scene change has taken place
+    public bool IsTransitioning { get; private set; } // true if we are in the process of changing scenes
+    bool didTransitionOut;
+    
+    public static System.Action OnTransition;         // called whenever a scene transition takes place
     // Start is called before the first frame update
     void Start()
     {
-        
+        transitionAnim = gameObject.GetComponent<Animator>();
+        if (SceneManager.GetActiveScene().name != "MainMenu")
+        {
+            transitionAnim.Play("OpenEyes");
+        }
     }
 
     // Update is called once per frame
@@ -18,9 +29,17 @@ public class ScenesTransition : MonoBehaviour
     }
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        transitionAnim.Play("CloseEyes");
+        LeanTween.delayedCall(gameObject, 4f, () =>
+        {
+            SceneManager.LoadSceneAsync(sceneName);
+        });
     }
 
+    private void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
     public void SetInactive(GameObject ob)
     {
         ob.SetActive(false);
