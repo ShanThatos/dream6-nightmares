795894d34acb1be7e62957ba5de5e7e4cc15c93d
Jiaxinhuang1 <60531514+Jiaxinhuang1@users.noreply.github.com>
2022-09-30 23:28:12 -0500

    alert for board

--- Ratpuncher/Assets/Scripts/OfficeManager.cs
+++ Ratpuncher/Assets/Scripts/OfficeManager.cs
@@ -11,7 +11,21 @@ public class OfficeManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (PlayerPrefs.GetInt("LadybirdSolved", 0) == 0)
+        {
+            boardAlert.SetActive(true);
+            ladybirdAlert.SetActive(true);
+        }
+        else if (PlayerPrefs.GetInt("ElioSolved", 0) == 0 && PlayerPrefs.GetInt("ElioActivated", 0) == 1)
+        {
+            boardAlert.SetActive(true);
+            elioAlert.SetActive(true);
+        }
+        else if (PlayerPrefs.GetInt("RemSolved", 0) == 0 && PlayerPrefs.GetInt("RemActivated", 0) == 1)
+        {
+            boardAlert.SetActive(true);
+            remAlert.SetActive(true);
+        }
     }
 
     // Update is called once per frame
