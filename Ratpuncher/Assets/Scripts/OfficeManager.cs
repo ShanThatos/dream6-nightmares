3289375d70a99d3f45b04f1d7956d7b24b40f729
Jiaxinhuang1 <60531514+Jiaxinhuang1@users.noreply.github.com>
2022-09-30 22:36:59 -0500

    new board for hub yay

---
+++ Ratpuncher/Assets/Scripts/OfficeManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfficeManager : MonoBehaviour
+{
+    public GameObject boardAlert;
+    public GameObject ladybirdAlert;
+    public GameObject elioAlert;
+    public GameObject remAlert;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
