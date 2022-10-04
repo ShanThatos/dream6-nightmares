b3780c9becdd983c4c7b57efb6987a31e295ad35
Neepsy <eric55664@gmail.com>
2022-10-03 22:50:30 -0500

    Factory level prototype

--- Ratpuncher/Assets/Characters/Player/DestroyOnPlayerAttack.cs
+++ Ratpuncher/Assets/Characters/Player/DestroyOnPlayerAttack.cs
@@ -21,5 +21,9 @@ public class DestroyOnPlayerAttack : MonoBehaviour
                 alreadyDestroyed = true;
             }
         }
+        else
+        {
+            Debug.Log("Wrong Layer");
+        }
     }
 }
