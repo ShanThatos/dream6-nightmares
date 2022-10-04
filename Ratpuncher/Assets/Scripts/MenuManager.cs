47bac402e07044ca4caabfd9926751981e6b6b94
Jiaxinhuang1 <60531514+Jiaxinhuang1@users.noreply.github.com>
2022-10-03 22:58:58 -0500

    ui navigation with keyboard in menu

--- Ratpuncher/Assets/Scripts/MenuManager.cs
+++ Ratpuncher/Assets/Scripts/MenuManager.cs
@@ -2,15 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     private bool isGameStart;
     public Animator canvasAnim;
+
+    [Header("We all love UI programming")]
+    public GameObject playButton;
+    public GameObject settingsButton;
+    public GameObject creditsButton;
+    public GameObject settingsFirstSelected;
+    public GameObject creditsFirstSelected;
+    public GameObject controlsFirstSelected;
+
+    [Header("Panels")]
+    public GameObject settingsPanel;
+    public GameObject creditsPanel;
+    public GameObject controlsPanel;
+
+    private bool backInput;
+    private bool oldBackInput;
+    private System.Action onBackInput;
+
     void Start()
     {
         isGameStart = false;
+        EventSystem.current.SetSelectedGameObject(null);
     }
 
     void Update()
@@ -20,6 +40,59 @@ public class MenuManager : MonoBehaviour
             StartGame();
             isGameStart = true;
         }
+
+        oldBackInput = backInput;
+        backInput = Input.GetAxisRaw("Cancel") > 0;
+
+        if (backInput && !oldBackInput)
+        {
+            onBackInput?.Invoke();
+        }
+    }
+
+    // It all starts here...
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(settingsFirstSelected);
+        onBackInput = CloseSettings;
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(settingsButton);
+        onBackInput = null;
+    }
+
+    public void OpenCredits()
+    {
+        creditsPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(creditsFirstSelected);
+        onBackInput = CloseCredits;
+    }
+
+    public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(creditsButton);
+        onBackInput = null;
+    }
+
+    public void OpenControls()
+    {
+        settingsButton.SetActive(false);
+        controlsPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(controlsFirstSelected);
+        onBackInput = CloseControls;
+    }
+
+    public void CloseControls()
+    {
+        controlsPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(settingsFirstSelected);
+        onBackInput = null;
     }
     public void LoadScene(string sceneName)
     {
@@ -35,6 +108,7 @@ public class MenuManager : MonoBehaviour
     {
         canvasAnim.Play("OpenMenuAnim");
         Debug.Log("StartGame");
+        EventSystem.current.SetSelectedGameObject(playButton);
     }
 
     public void ButtonSelect(GameObject go)
@@ -45,5 +119,15 @@ public class MenuManager : MonoBehaviour
     public void ButtonDeselect(GameObject go)
     {
         LeanTween.scale(go, new Vector3(1f, 1f, 1f), 0.2f);
+    }
+
+    public void SliderSelect(GameObject go)
+    {
+        LeanTween.scale(go, new Vector3(4.2f, 3.2f, 4.2f), 0.2f);
+    }
+
+    public void SliderDeselect(GameObject go)
+    {
+        LeanTween.scale(go, new Vector3(4f, 3f, 4f), 0.2f);
     }
 }
