fd719ebe4edd792f8a19bee3c7ffafeb6fae963d
Jiaxinhuang1 <60531514+Jiaxinhuang1@users.noreply.github.com>
2022-10-10 13:22:15 -0500

    change key

--- Ratpuncher/Assets/Scripts/Triggers/TutorialIndicator.cs
+++ Ratpuncher/Assets/Scripts/Triggers/TutorialIndicator.cs
@@ -15,11 +15,44 @@ public class TutorialIndicator : MonoBehaviour
     private bool isTriggered;
     private GameObject actionIndicator;
     // Start is called before the first frame update
+    private void Start()
+    {
+        KeyChange();
+    }
+    private void KeyChange()
+    {
+        if (inputAction != null)
+        {
+            if (string.IsNullOrWhiteSpace(actionOverride))
+            {
+                actionOverride = inputAction.action.name;
 
+            }
+
+            if (string.IsNullOrWhiteSpace(keyOverride))
+            {
+                if (actionOverride.ToUpper() == "MOVE")
+                {
+                    // Need to handle a special case for movement since its a composite
+                    keyOverride = inputAction.action.GetBindingDisplayString(5);
+                }
+                else
+                {
+                    keyOverride = inputAction.action.GetBindingDisplayString();
+                }
+
+                if (isDoublePress)
+                {
+                    keyOverride += "x2";
+                }
+            }
+        }
+    }
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            KeyChange();
             if (!isTriggered)
             {
                 actionIndicator = Instantiate(Resources.Load("ActionIndicator")) as GameObject;
@@ -39,36 +72,6 @@ public class TutorialIndicator : MonoBehaviour
         {
             isTriggered = false;
             LeanTween.scaleY(actionIndicator, 0, 0.2f).setDestroyOnComplete(true);
-        }
-    }
-
-    private void Start()
-    {
-        if(inputAction != null)
-        {
-            if(string.IsNullOrWhiteSpace(actionOverride))
-            {
-                actionOverride = inputAction.action.name;
-
-            }
-            
-            if(string.IsNullOrWhiteSpace(keyOverride))
-            {
-                if(actionOverride.ToUpper() == "MOVE")
-                {
-                    // Need to handle a special case for movement since its a composite
-                    keyOverride = inputAction.action.GetBindingDisplayString(5);
-                }
-                else
-                {
-                    keyOverride = inputAction.action.GetBindingDisplayString();
-                }
-
-                if (isDoublePress)
-                {
-                    keyOverride += "x2";
-                }
-            }
         }
     }
 }
